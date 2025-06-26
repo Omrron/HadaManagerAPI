64bc5505961f11136defcec860a159c35aef6dc1
Omrron <obaron4120@gmail.com>
2025-06-26 19:37:46 +0300

    Getting base controller working and connected to DB

---
+++ HadaManagerAPI/HadaManagerAPI/DB/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using HadaManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HadaManagerAPI.DB
+{
+    public sealed class DBContext : DbContext
+    {
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        public string DbPath;
+
+        public DBContext(DbContextOptions<DBContext> options)
+        : base(options)
+        {
+        }
+    }
+}
